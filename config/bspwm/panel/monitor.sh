d19c04d024d0dbfcdeb94a9be7da35ea60074292
1995eaton <1995eaton@gmail.com>
2014-02-26 01:55:17 -0500

    conky updated

--- config/bspwm/panel/monitor.sh
+++ config/bspwm/panel/monitor.sh
@@ -10,12 +10,13 @@ get_active_mon() {
 
 get_mons() {
   mons=`bspc query -D`
+  mons=`echo $mons`
 }
 
 format_output() {
   get_active_mon
   get_mons
-  echo $mons | sed "s/\($active_mon\)/\\\f2\1\\\f\\\/"
+  echo $mons | sed 's/ /  /g' | sed 's/\(^\|$\)/ /g' | sed "s/ \($active_mon\) /\\\f9\\\u1\\\b1 \1 \\\u \\\\\\b\\\\\ \\\f/g"
 }
 
 format_output
