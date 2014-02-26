ea5b3a4c29d921d9ef49d6465283adaff30fef5e
1995eaton <1995eaton@gmail.com>
2014-02-26 02:06:44 -0500

    more updates...

--- config/bspwm/panel/monitor.sh
+++ config/bspwm/panel/monitor.sh
@@ -16,7 +16,8 @@ get_mons() {
 format_output() {
   get_active_mon
   get_mons
-  echo $mons | sed 's/ /  /g' | sed 's/\(^\|$\)/ /g' | sed "s/ \($active_mon\) /\\\f9\\\u1\\\b1 \1 \\\u \\\\\\b\\\\\ \\\f/g"
+  #echo $mons | sed 's/ /  /g' | sed 's/\(^\|$\)/ /g' | sed "s/ \($active_mon\) /\\\f9\\\u1\\\b1 \1 \\\u \\\\\\b\\\\\ \\\f/g"
+  echo $mons | sed 's/ /  /g' | sed 's/\(^\|$\)/ /g' | sed "s/ \($active_mon\) /\\\f9\\\b1 \1 \\\\\\b\\\\\ \\\f/g"
 }
 
 format_output
